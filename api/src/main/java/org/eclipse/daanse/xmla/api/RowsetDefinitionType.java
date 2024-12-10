039fccac06f3e2a929c8880c7dbc1f8f74cec370
Stefan Bischof <stbischof@bipolis.org>
2024-12-10 15:24:01 +0100

    fix ci issues
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- api/src/main/java/org/eclipse/daanse/xmla/api/RowsetDefinitionType.java
+++ api/src/main/java/org/eclipse/daanse/xmla/api/RowsetDefinitionType.java
@@ -1,14 +1,19 @@
 /*
-// This software is subject to the terms of the Eclipse Public License v1.0
-// Agreement, available at the following URL:
-// http://www.eclipse.org/legal/epl-v10.html.
-// You must accept the terms of that agreement to use this software.
-//
-// Copyright (C) 2003-2005 Julian Hyde
-// Copyright (C) 2005-2018 Hitachi Vantara
-// Copyright (C) 2019 Topsoft
-// Copyright (C) 2020 - 2022 Sergei Semenkov
-// All Rights Reserved.
+* Copyright (c) 2023 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Stefan Bischof (bipolis.org)
+*   Copyright (C) 2003-2005 Julian Hyde
+*   Copyright (C) 2005-2017 Hitachi Vantara
+*   Copyright (C) 2019 Topsoft
+*   Copyright (C) 2020 - 2022 Sergei Semenkov
+*   All Rights Reserved.
 */
 
 package org.eclipse.daanse.xmla.api;
